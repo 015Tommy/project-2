5a575b66d7c2cc7acb3654b64abb0e396efe638b
015Tommy <302181697@student.rocmondriaan.nl>
2024-12-18 14:28:52 +0100

    draggable add

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ const divs = document.querySelectorAll('div');
 
 for (let i = 0; i < divs.length; i++) {
     const div = divs[i];
-     if (div.classList.contains('boxes')) {
+    if (div.classList.contains('boxes')) {
         console.log('Found a boxes div:', div);
     }
 }
 
 
+const ships = document.querySelectorAll('.ship');
+ships.forEach(ship => {
+    ship.setAttribute('draggable', 'true');
+});
